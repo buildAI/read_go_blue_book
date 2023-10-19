56d5bef4b89f50345b03753316d19a740a4e9a33
Karthik Yelchuru <yelkarthik@gmail.com>
2023-10-19 15:51:33 +0530

    Modify dup logic to read from file in a single shot and then update the data instead of contiuous stream

--- ch1/dup/main.go
+++ ch1/dup/main.go
@@ -4,24 +4,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
 
@@ -29,13 +26,5 @@ func main() {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
-	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-
-	for input.Scan() {
-		counts[input.Text()]++
 	}
 }
